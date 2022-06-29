4fff04fa9f53f3bc6c775ef387e890306cef288c
iambowcreek <105578226+iambowcreek@users.noreply.github.com>
2022-06-28 18:29:09 -0600

    Update common1.groovy

--- vars/common1.groovy
+++ vars/common1.groovy
@@ -14,7 +14,7 @@ def call(String stageName){
      }
   else if ("${stageName}" == "Deploy to UAT")
      {
-       sh "mvn scp target/*.war ec2-user@18.237.101.171:/opt/tomcat9/webapps"
+       sh "mvn scp target/maven-web-app.war ec2-user@18.237.101.171:/opt/tomcat9/webapps"
      }
   else if ("${stageName}" == "Approval Gate")
      {
